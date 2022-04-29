fd4bdd569c12a7329b6e73af8173304631848041
amina-seraoui <amina.seraoui@outlook.fr>
2022-04-29 14:32:32 +0200

    :bug: try to fix vercel issue

--- next.config.js
+++ next.config.js
@@ -13,8 +13,7 @@ const nextConfig = {
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
     measurementId: process.env.FIREBASE_MEASUREMENT_ID
-  },
-  swcMinify: false
+  }
 }
 
 module.exports = nextConfig
