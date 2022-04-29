4b0ab78626e55b3a3b228790049415ef859438b7
amina-seraoui <amina.seraoui@outlook.fr>
2022-04-29 14:26:16 +0200

    :bug: try to fix deployment error

--- next.config.js
+++ next.config.js
@@ -13,7 +13,8 @@ const nextConfig = {
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID,
     measurementId: process.env.FIREBASE_MEASUREMENT_ID
-  }
+  },
+  swcMinify: false
 }
 
 module.exports = nextConfig
