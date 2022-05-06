c45d4efe11150da2c42d12da625369315ed2beec
amina-seraoui <amina.seraoui@outlook.fr>
2022-05-06 15:32:50 +0200

    :hammer: email notification

--- src/pages/api/msg.js
+++ src/pages/api/msg.js
@@ -1,13 +1,41 @@
 import { create } from '../../js/model'
+const nodemailer = require('nodemailer')
 
 export default function handler(req, res) {
-    create('messages', req.body)
-        .then(doc => {
-            console.log(doc)
-            res.status(200).json({success: true})
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500)
-        })
+    if (req.method === 'POST') {
+        create('messages', req.body)
+            .then(doc => {
+                sendMail()
+                res.status(200).json({success: true})
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500)
+            })
+    } else res.status(403).send('bad request')
+}
+
+const sendMail = () => {
+    const config = {
+        service: 'gmail',
+        auth: {
+            user: process.env.MAIL_AUTH_USER,
+            pass: process.env.MAIL_AUTH_PASS
+        }
+    }
+
+    const mail = {
+        from: process.env.MAIL_FROM,
+        to: process.env.MAIL_FROM,
+        subject: 'Nuovo Messaggio',
+        text: 'Buongiorno | Buondi'
+    }
+
+    const transporter = nodemailer.createTransport(config)
+
+    transporter.sendMail(mail, (err, info) => {
+        if (err) throw err
+        console.log('Email sent !')
+        console.log(info)
+    })
 }
